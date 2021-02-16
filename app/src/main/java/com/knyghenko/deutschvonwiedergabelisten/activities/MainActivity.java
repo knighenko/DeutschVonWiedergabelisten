19f16a88fcc0cad2425e8cd22a0d62eed8890d60
Maksym Knyzhenko <knighenko@gmail.com>
2021-02-16 21:40:44 +0200

    Add some changes

--- app/src/main/java/com/knyghenko/deutschvonwiedergabelisten/activities/MainActivity.java
+++ app/src/main/java/com/knyghenko/deutschvonwiedergabelisten/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.knyghenko.deutschvonwiedergabelisten.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.knyghenko.deutschvonwiedergabelisten.R;
 
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnEnter(View view) {
+    }
+
+    public void createUser(View view) {
     }
 }
