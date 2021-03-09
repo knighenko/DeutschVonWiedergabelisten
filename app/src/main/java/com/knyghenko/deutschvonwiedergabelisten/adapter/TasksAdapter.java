31a422dab12858c2ed3e7543cd9d54f3571773d9
Maksym Knyzhenko <knighenko@gmail.com>
2021-03-09 22:21:21 +0200

    implemented TasksAdapter.java

--- app/src/main/java/com/knyghenko/deutschvonwiedergabelisten/adapter/TasksAdapter.java
+++ app/src/main/java/com/knyghenko/deutschvonwiedergabelisten/adapter/TasksAdapter.java
@@ -1,4 +1,34 @@
 package com.knyghenko.deutschvonwiedergabelisten.adapter;
 
-public class TasksAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.ViewHolder>{
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+         ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
