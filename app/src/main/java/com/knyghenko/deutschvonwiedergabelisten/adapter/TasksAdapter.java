b70a8994a96db96c2a2ab3807e173c0129e0b643
Maksym Knyzhenko <knighenko@gmail.com>
2021-03-11 08:37:09 +0200

    implemented TasksAdapter.java

--- app/src/main/java/com/knyghenko/deutschvonwiedergabelisten/adapter/TasksAdapter.java
+++ app/src/main/java/com/knyghenko/deutschvonwiedergabelisten/adapter/TasksAdapter.java
@@ -2,18 +2,25 @@ package com.knyghenko.deutschvonwiedergabelisten.adapter;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.knyghenko.deutschvonwiedergabelisten.R;
+import com.knyghenko.deutschvonwiedergabelisten.activities.TaskActivity;
 import com.knyghenko.deutschvonwiedergabelisten.activities.TasksList;
 import com.knyghenko.deutschvonwiedergabelisten.entity.Lesson;
 import com.knyghenko.deutschvonwiedergabelisten.model.ConnectServer;
 
-public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.ViewHolder>{
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.ViewHolder> {
 
 
     @NonNull
@@ -33,40 +40,36 @@ public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.ViewHolder>{
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textViewTask;
+        private final EditText textViewAnswer;
+        private final Button buttonSubmit;
+        private final Button buttonHelp;
+        private final ImageView imageCheck;
 
-         ViewHolder(@NonNull View itemView) {
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
+            textViewTask = (TextView) itemView.findViewById(R.id.textViewTask);
+            textViewAnswer = (EditText) itemView.findViewById(R.id.editTextMyAnswer);
+            buttonSubmit = (Button) itemView.findViewById(R.id.buttonSubmit);
+            buttonHelp = (Button) itemView.findViewById(R.id.buttonHelp);
+            imageCheck=(ImageView)itemView.findViewById(R.id.imageCheck);
         }
 
-    } public void bind(Lesson lesson) {
-        titleLesson.setText(lesson.getTitle());
-        imageViewLesson.setImageResource(R.drawable.lesson);
-        imageDownload.setImageResource(R.drawable.download);
-        imageViewLesson.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String tasks = ConnectServer.connectToServerSearch("3:" + lesson.getId());
-                Intent intent = new Intent(itemView.getContext(), TasksList.class);
-                intent.putExtra("tasks", tasks);
-                intent.putExtra("lessonTitle", lesson.getTitle());
-                itemView.getContext().startActivity(intent);
-            }
-        });
-        imageYouTube.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String url = lesson.getYouTubeUrl();
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(url));
-                itemView.getContext().startActivity(intent);
-            }
-        });
-        imageDownload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onLessonClickListener.onAdvClick(lesson);
 
-            }
-        });
+        public void bind(String task, String answer) {
+            textViewTask.setText(task);
+          buttonSubmit.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  String myAnswer=textViewAnswer.getText().toString();
+                  if myAnswer.equals(answer){
+                     imageCheck
+                             .setVisibility(View.VISIBLE);
+                  }
+              }
+          });
+
+
+        }
     }
 }
